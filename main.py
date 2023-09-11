71e682122ebef8a75e4ebfe0980171638527ab4f
IoSoNiko <45022699+IoSoNiko@users.noreply.github.com>
2023-09-11 18:04:23 +0000

    storage

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types.reply_keyboard import ReplyKeyboardRemove
+from pyflipper.pyflipper import PyFlipper
+
+
 
 # Imposta il livello di registro su DEBUG
 logging.basicConfig(level=logging.DEBUG)
@@ -26,13 +29,14 @@ bot = Bot(token=BOT_TOKEN)
 # Crea un'istanza del dispatcher
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+flipper = PyFlipper(com="/dev/ttyACM0")
 
 
 # Funzione per creare il layout dei pulsanti inline
 def get_inline_keyboard():
     keyboard = InlineKeyboardMarkup()
     keyboard.row(
-        InlineKeyboardButton('Pulsante 1', callback_data='button1'),
+        InlineKeyboardButton('Storage Info', callback_data='infoStorage'),
         InlineKeyboardButton('Pulsante 2', callback_data='button2')
     )
     return keyboard
@@ -62,10 +66,17 @@ async def send_welcome(message: types.Message):
     await message.reply("Ciao! Premi il pulsante qui sotto per continuare:", reply_markup=get_inline_keyboard())
 
 # Gestione del callback dei pulsanti inline
-@dp.callback_query_handler(lambda c: c.data in ['button1', 'button2'])
-async def process_callback_button1(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(lambda c: c.data in ['infoStorage', 'button2'])
+async def process_callback_buttons(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, f"Hai premuto il pulsante {callback_query.data}")
+    
+    
+
+    if 'infoStorage' == callback_query.id:
+        ext_info = flipper.storage.info(fs="/ext")
+        await bot.send_message(callback_query.from_user.id, f"Info Storage: {ext_info}")
+    else:
+        await bot.send_message(callback_query.from_user.id, f"Hai premuto il pulsante {callback_query.data}")
 
     
     
@@ -73,7 +84,10 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
 if __name__ == '__main__':
     # Avvia il bot
     try:
+        
         logging.info("Starting bot")
         executor.start_polling(dp)
+
+
     except Exception as e:
         logging.exception(e)
