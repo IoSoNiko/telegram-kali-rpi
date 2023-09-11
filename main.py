91168695e87295e569adae67bf9a29623e03200a
IoSoNiko <45022699+IoSoNiko@users.noreply.github.com>
2023-09-11 18:08:24 +0000

    flipper.power.off()

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ async def process_callback_buttons(callback_query: types.CallbackQuery):
         ext_info = flipper.storage.info(fs="/ext")
         await bot.send_message(callback_query.from_user.id, f"Info Storage: {ext_info}")
     else:
+        flipper.power.off()
         await bot.send_message(callback_query.from_user.id, f"Hai premuto il pulsante {callback_query.data}")
 
     
