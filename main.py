097e21bc57d2b14a075b2d7fda4bcfde52a6cc16
IoSoNiko <45022699+IoSoNiko@users.noreply.github.com>
2023-09-15 02:53:28 +0000

    msg

--- main.py
+++ main.py
@@ -110,73 +110,60 @@ async def process_callback_buttons(callback_query: types.CallbackQuery):
     if 'appList' == callback_query.data:
         apps = flipper.loader.list()
         #flipper.loader.open(app_name="Clock")
-        await bot.send_message(callback_query.from_user.id, f"App List: {apps}")
+        await bot.edit_message_text(chat_id=callback_query.from_user.id,message_id=mess_response, text= f"App List: {apps}")
     elif 'powerOff' == callback_query.data:
         await flipper.power.off()
-        await bot.send_message(callback_query.from_user.id, f"Hai premuto il pulsante {callback_query.data}")
+        await bot.edit_message_text(chat_id=callback_query.from_user.id,message_id=mess_response, text=f"Hai premuto il pulsante {callback_query.data}")
     elif 'reboot' == callback_query.data:
         await flipper.power.reboot()
-        await bot.send_message(callback_query.from_user.id, f"Rebooting...")
+        await bot.edit_message_text(chat_id=callback_query.from_user.id,message_id=mess_response, text=f"Rebooting...")
     elif 'ext_tree' == callback_query.data:
         #ext_tree = flipper.storage.tree(path="/ext")
         ext_list = flipper.storage.list(path="/ext")
+        await bot.edit_message_text(chat_id=callback_query.from_user.id,message_id=mess_response, text=f"Ext Tree: {ext_list}")
     elif 'subghz_chart' == callback_query.data:
-        await start_subghz_listener(message)
-        
+        msg  = await bot.send_message(chat_id=callback_query.from_user.id, "Listener Sub-GHz terminato.")
+        # Specifica la frequenza Sub-GHz desiderata (ad esempio, 433920000)
+        subghz_frequency = 433920000
+        # Avvia il listener
+        flipper.subghz.rx(frequency=subghz_frequency, raw=True)
+        # Inizia a ricevere dati per un certo periodo di tempo (ad esempio, 5 secondi)
+        timeout = 5
+        start_time = time.time()
 
-        await bot.send_message(callback_query.from_user.id, f"Ext Tree: {ext_list}")
-
-
-#Get the storage /ext tree dict
-
-async def start_subghz_listener(message: types.Message):
-    # Verifica che l'utente abbia i permessi necessari per avviare il listener Sub-GHz
-    if message.from_user.id not in [33033257, 1138794081]:  # Sostituisci questi numeri con gli ID degli utenti autorizzati
-        return
-
-    # Usa PyFlipper per avviare il listener Sub-GHz
-    flipper = PyFlipper(com="/dev/ttyACM0")
-
-    # Specifica la frequenza Sub-GHz desiderata (ad esempio, 433920000)
-    subghz_frequency = 433920000
-
-    # Avvia il listener
-    flipper.subghz.rx(frequency=subghz_frequency, raw=True)
-
-    # Inizia a ricevere dati per un certo periodo di tempo (ad esempio, 5 secondi)
-    timeout = 5
-    start_time = time.time()
-
-    while time.time() - start_time < timeout:
-        data = flipper.subghz.rx(frequency=subghz_frequency, raw=True)
-        
-        if data:
-            # Invia i dati ricevuti come messaggio Telegram
-            for data_str in data.replace("Listening at 433919830. Press CTRL+C to stop"," ").split("\r\n\n"):
-                data_values = [int(value) for value in data_str.split()]
-                
-                # Crea il grafico
-                plt.figure(figsize=(10, 6))
-                plt.plot(data_values)
-                plt.title('Dati ricevuti dal Sub-GHz')
-                plt.xlabel('Campione')
-                plt.ylabel('Valore')
-                plt.grid(True)
+        while time.time() - start_time < timeout:
+            data = flipper.subghz.rx(frequency=subghz_frequency, raw=True)
             
+            if data:
+                # Invia i dati ricevuti come messaggio Telegram
+                for data_str in data.replace("Listening at 433919830. Press CTRL+C to stop"," ").split("\r\n\n"):
+                    data_values = [int(value) for value in data_str.split()]
+                    
+                    # Crea il grafico
+                    plt.figure(figsize=(10, 6))
+                    plt.plot(data_values)
+                    plt.title('Dati ricevuti dal Sub-GHz')
+                    plt.xlabel('Campione')
+                    plt.ylabel('Valore')
+                    plt.grid(True)
+                
 
-                # Salva il grafico in un buffer
-                buffer = BytesIO()
-                plt.savefig(buffer, format='png')
-                buffer.seek(0)
-                try:
-                    await message.reply_photo(photo=buffer, caption='Grafico dei dati ricevuti dal Sub-GHz')
-                except Exception as e:
-                    print(f"Errore nell'invio dell'immagine: {e}")
+                    # Salva il grafico in un buffer
+                    buffer = BytesIO()
+                    plt.savefig(buffer, format='png')
+                    buffer.seek(0)
+                    try:
+                        await msg.reply_photo(photo=buffer, caption='Grafico dei dati ricevuti dal Sub-GHz')
+                    except Exception as e:
+                        print(f"Errore nell'invio dell'immagine: {e}")
 
-                # Chiudi la figura di Matplotlib
-                plt.close()
+                    # Chiudi la figura di Matplotlib
+                    plt.close()
 
-    await message.reply("Listener Sub-GHz terminato.")
+        await msg.reply("Listener Sub-GHz terminato.")
+        
+
+
     
 
 if __name__ == '__main__':
