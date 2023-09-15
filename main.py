1431d0cefdd60daa818f6e2e47746c6df839fc48
IoSoNiko <45022699+IoSoNiko@users.noreply.github.com>
2023-09-15 03:12:14 +0000

    3

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def process_callback_buttons(callback_query: types.CallbackQuery):
         # Avvia il listener
         #flipper.subghz.rx(frequency=subghz_frequency, raw=True)
         # Inizia a ricevere dati per un certo periodo di tempo (ad esempio, 5 secondi)
-        timeout = 22 s
+        timeout = 2
         start_time = time.time()
 
         #while time.time() - start_time < timeout:
